f229cab37a9472eb435cd0586d762499f72bc731
juan42006 <schull.surf@gmail.com>
2025-11-21 18:21:19 -0500

    Add files via upload

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                char[] c;
+                Console.Write("* Ingresa mensaje: ");
+                c = Console.ReadLine().ToCharArray();
+                Console.WriteLine();
+                Console.WriteLine("* Mensaje ingresado.");
+                Console.Write("c[{0}] = ", c.Length);
+                for (int i = 0; i < c.Length; i++)
+                {
+                    Console.Write("[{0}]", c[i]);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+    
